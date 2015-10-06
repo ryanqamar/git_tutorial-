2b077e11eae3bd36d6221d54bbb6edba6970fbda
Jason Merlo <macman320@gmail.com>
2015-10-06 16:17:15 -0400

    Git workshop skeleton code
    Skeleton of Python calculator to be used during the workshop

---
+++ Calculator_1.py
@@ -0,0 +1,10 @@
+__author__ = 'jasonmerlo'
+
+
+def main():
+    num_a = int(input("Enter a number to add "))
+    num_b = int(input("Enter another number to add: "))
+    print("{}".format(num_a + num_b))
+
+
+main()
