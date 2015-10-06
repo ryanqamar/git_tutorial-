b02e79f25bfd014e0922bdc32d083c6e1842b2f4
Ryan Qamar <qamarrya@msu.edu>
2015-10-06 19:56:03 -0400

    Added subtraction feature

---
+++ Calculator1.py
@@ -0,0 +1,11 @@
+def main():
+	num_a = int(input("Enter a number to add"))
+	num_b = int(input("Enter another number to add: "))
+	print("{}".format(num_a + num+b))
+
+	num_a = int(input("Enter a number to subtract from: "))
+	num_b = int(input("Enter a second number to subtract: "))
+	print("{}".format(num_a-num_b))
+
+main()
+
